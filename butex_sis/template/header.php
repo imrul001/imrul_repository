b6d4a94645cdf58456d2c2d4a3bafdb6fc930bd7
imrul <imrul@imrul-U30Jc.(none)>
2013-12-21 18:20:45 +0600

    BUTEX_SIS: add google analytics ..
    Signed-off-by: imrul <imrul@imrul-U30Jc.(none)>

--- butex_sis/template/header.php
+++ butex_sis/template/header.php
@@ -10,6 +10,19 @@
         <script type="text/javascript" src="/min/?f=template/js/jquery-ui.js" ></script>
         <script type="text/javascript" src="/min/?f=template/js/jquery.calendar.js"></script>
         <link rel="icon" type="image/png" href="/template/images/favicon.ico" />
+
+        <!--Google analytics-->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-46663537-1', 'butex.edu.bd');
+            ga('send', 'pageview');
+
+        </script>
+        <!--Google analytics-->
         <script type="text/javascript">
             $(document).ready(function(){
                 $('.subButton').hover(function(){
